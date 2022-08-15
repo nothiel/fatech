9677736bae8b6e27609e99406bbafb8445d10d62
Rafael Barbosa <rafaellb2555@gmail.com>
2022-08-15 10:49:49 -0300

    Add fetch on local server

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-
+import axios from 'axios';
 export default function App() {
-  const { register, handleSubmit } = useForm();
-  function onSubmit(data) {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  function _calculateAge(birthday) { // birthday is a date
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+  const onSubmit = data => {
     console.log(data)
+    data.age = _calculateAge(new Date(data.birth_date))
+    // console.log(data)
+    axios.post('http://localhost:8000/user/register', data).then(response => response.json()).then(data => console.log(data))
   }
   
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input type="text" placeholder="Nome Completo" {...register("name", {required: true, maxLength: 80})} />
       <input type="text" placeholder="Email" {...register("email", {required: true, pattern: /^\S+@\S+$/i})} />
-      <input type="datetime" placeholder="Data de Nascimento" {...register("birthDate", {required: true})} />
+      <input type="date" placeholder="Data de Nascimento" {...register("birth_date", {required: true})} />
       <select {...register("gender", { required: true })}>
         <option value="Masculino">Masculino</option>
         <option value="Feminino">Feminino</option>
