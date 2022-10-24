dbbc50fb2fc4d4cac933ac453c0fe59c0f4d0bca
Rafael Barbosa <rbarbosa@axcient.com>
2022-10-23 21:19:53 -0300

    Feat: Add new login page, add auth and storage hook

--- src/App.js
+++ src/App.js
@@ -1,41 +1,18 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
-import axios from 'axios';
-export default function App() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+// import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard';
+import Login from './components/Login/Login';
+import { ProtectedRoute } from './utils/protectedRoute';
+// import Register from '../Register/Register';
 
-  function _calculateAge(birthday) { // birthday is a date
-    var ageDifMs = Date.now() - birthday.getTime();
-    var ageDate = new Date(ageDifMs); // miliseconds from epoch
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-  }
-  const onSubmit = data => {
-    console.log(data)
-    data.age = _calculateAge(new Date(data.birth_date))
-    // console.log(data)
-    axios.post('http://localhost:8000/user/register', data).then(response => response.json()).then(data => console.log(data))
-  }
-  
+function App() {
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="Nome Completo" {...register("name", {required: true, maxLength: 80})} />
-      <input type="text" placeholder="Email" {...register("email", {required: true, pattern: /^\S+@\S+$/i})} />
-      <input type="date" placeholder="Data de Nascimento" {...register("birth_date", {required: true})} />
-      <select {...register("gender", { required: true })}>
-        <option value="Masculino">Masculino</option>
-        <option value="Feminino">Feminino</option>
-        <option value="Não Binário/Prefiro não declarar">Não Binário/Prefiro não declarar</option>
-      </select>
-      <select {...register("education", { required: true })}>
-        <option value="Superior completo">Superior completo</option>
-        <option value="Superior incompleto">Superior incompleto</option>
-        <option value="Ensino médio completo">Ensino médio completo</option>
-        <option value="Ensino médio incompleto">Ensino médio incompleto</option>
-        <option value="Ensino fundamental completo">Ensino fundamental completo</option>
-        <option value="Ensino fundamental incompleto">Ensino fundamental incompleto</option>
-      </select>
-
-      <input type="submit" />
-    </form>
+      <Routes>
+          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="/login" element={<Login/>} />
+      </Routes>
   );
 }
+
+export default App;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import './Login.css';
+import logo from '../../assets/img/logo.png';
+import { useForm } from 'react-hook-form';
+
+export default function Login() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data)
+        console.log(errors)
+    }
+    return (
+        <div className="loginBody">
+        <div className="container">
+            <div className="form-image">  
+                <img src={logo}/>
+            </div>
+            <div className="form">
+                <form action="#"  onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-header">
+                        <div className="title">
+                            <h1>Login</h1>
+                        </div>
+                        <div className="register-button">
+                            <button><a href="/register">Inscreva-se</a></button>
+                        </div>
+                    </div>
+                    <div className="input-group">
+                       <div className="input-box">
+                            <label htmlFor="email">Email</label>
+                            <input id="email"type="email" name="email" placeholder="Digite seu email" {...register("email", {required: true, pattern: /^\S+@\S+$/i})}/>
+                       </div>
+                        <div className="input-box">
+                            <label htmlFor="password">Senha</label>
+                            <input id="password"type="password" name="password" placeholder="Digite sua senha" {...register("birth_date", {required: true})}/>
+                        </div>
+                    </div>
+                    <div className="continue-button">
+                        <input type="submit" />
+                    </div>
+                </form>
+            </div>
+        </div>
+        </div>
+    )
+}
